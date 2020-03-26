a51f5a44e82c522c2f887465e477aecd15644f86
DARK <darksoul2407@gmail.com>
2020-03-27 00:33:12 +0400

    v1.2 среднее напряжения и сумма тока считает. Делаем проверку на пустоту полей.

--- app/src/main/java/example/firsttest/MainActivity.java
+++ app/src/main/java/example/firsttest/MainActivity.java
@@ -12,104 +12,166 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import org.w3c.dom.Text;
 
-    private Chronometer mChronometer;
-    private TextView timerValue;
-    private long startTime = 0L;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    long startTime = 0;
+    TextView timerTextView,Mosnost,Itog,pogresnost;
     private Handler customHandler = new Handler();
-    long timeInMilliseconds = 0L;
-    long timeSwapBuff = 0L;
-    long updatedTime = 0L;
+    String  pTime;
+
+    TextView tv5;
+    EditText Ampers,Impuls,Postoyannay,Volts,Time,et_V1, et_V2,et_V3;
+    Button SummaTok,SredVolts;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - startTime;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            long milliseconds = millis % 1000;
+            timerTextView.setText(String.format("%d:%02d:%02d", minutes, seconds, milliseconds));
+            timerHandler.postDelayed(this, 0);
+            pTime = timerTextView.getText().toString();
+            ParseTime();
+        }
+    };
+
+    private void ParseTime() {
+      //  Toast.makeText(this,pTime,Toast.LENGTH_LONG).show();
+        String min = pTime.split(":")[0];
+        String sec =  pTime.split(":")[1];
+        String mil = pTime.split(":")[2];
+       // Toast.makeText(this,min+sec+mil,Toast.LENGTH_LONG).show();
+        String vremy = sec + "." + mil;
+     //   Toast.makeText(this,vremy,Toast.LENGTH_LONG).show();
+        final EditText Time = findViewById(R.id.editTextTime);
+        Time.setText(vremy);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView timerValue = findViewById(R.id.tv_timer_value);
-
         Button ButtonRaschitat = findViewById(R.id.buttonRaschitat);
         Button startButton = findViewById(R.id.btn_start);
-        Button pauseButton = findViewById(R.id.btn_pause);
-        Button stopButton = findViewById(R.id.btn_stop);
+        SummaTok = findViewById(R.id.buttonSumTok);
+        SredVolts = findViewById(R.id.buttonSredVolts);
         startButton.setOnClickListener(this);
-        pauseButton.setOnClickListener(this);
-        stopButton.setOnClickListener(this);
         ButtonRaschitat.setOnClickListener(this);
+        SummaTok.setOnClickListener(this);
+        SredVolts.setOnClickListener(this);
 
+        timerTextView = findViewById(R.id.timerTextView);
+        Ampers = findViewById(R.id.editTextAmpers);
+        et_V1 = findViewById(R.id.editText1);
+        et_V2 = findViewById(R.id.editText2);
+        et_V3 = findViewById(R.id.editText3);
 
+        Impuls = findViewById(R.id.editTextImpuls);
+        Postoyannay = findViewById(R.id.editTextPostoyannay);
+        Volts = findViewById(R.id.editTextVolts);
+        Time = findViewById(R.id.editTextTime);
+
+        Mosnost = findViewById(R.id.textViewMosnost);
+        Itog = findViewById(R.id.textViewItog);
+        pogresnost = findViewById(R.id.textViewPogresnost);
+        tv5 = findViewById(R.id.textView5);
+
+        Button b =  findViewById(R.id.btn_start);
+        b.setText("start");
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText().equals("stop")) {
+                    timerHandler.removeCallbacks(timerRunnable);
+                    b.setText("start");
+                } else {
+                    startTime = System.currentTimeMillis();
+                    timerHandler.postDelayed(timerRunnable, 0);
+                    b.setText("stop");
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_start:
-                try {
-                    startTime = SystemClock.uptimeMillis();
-                    customHandler.postDelayed(updateTimerThread, 0);
-                }catch (Exception e) {
-                    Toast.makeText(this,"ошибка тут",Toast.LENGTH_LONG).show();
-                }
-
-
-                break;
-            case R.id.btn_pause:
-                timeSwapBuff += timeInMilliseconds;
-                customHandler.removeCallbacks(updateTimerThread);
-                break;
-            case R.id.btn_stop:
-                startTime = 0L;
-                customHandler.removeCallbacks(updateTimerThread);
-                timerValue.setText(getString(R.string.timer_val));
-                break;
+        switch (v.getId()) {
             case R.id.buttonRaschitat:
-                EditText Impuls = findViewById(R.id.editTextImpuls);
-                EditText Postoyannay = findViewById(R.id.editTextPostoyannay);
-                final EditText Volts = findViewById(R.id.editTextVolts);
-                final EditText Ampers = findViewById(R.id.editTextAmpers);
-                final EditText Time = findViewById(R.id.editTextTime);
+                try {
 
-                final TextView Mosnost = findViewById(R.id.textViewMosnost);
-                final TextView Itog = findViewById(R.id.textViewItog);
-                final TextView pogresnost = findViewById(R.id.textViewPogresnost);
-                // учтенная мощность
-                int n = Integer.parseInt(Impuls.getText().toString());
-                int a = Integer.parseInt(Postoyannay.getText().toString());
-                float t = Float.parseFloat(Time.getText().toString());
-                float pogresn = (3600 * n / a * t) * 100;
-                String s = Float.toString(pogresn);
-                pogresnost.setText(s);
+                    // учтенная мощность
+                    if (Time.getText().equals("")) {
+                        Toast.makeText(this, "проверьте импульсы", Toast.LENGTH_LONG).show();
+                    }
+                    if (Volts.getText().equals("")) {
+                        Toast.makeText(this, "проверьте импульсы", Toast.LENGTH_LONG).show();
+                    }
+                    int n = Integer.parseInt(Impuls.getText().toString());
+                    int a = Integer.parseInt(Postoyannay.getText().toString());
+                    float t = Float.parseFloat(Time.getText().toString());
+                    float pogresn = ((3600 * n) / (a * t)) * 1000;
+                    String s = Float.toString(pogresn);
+                    pogresnost.setText(s);
 
 
-                //общая мощность
-                float A = Float.parseFloat(Ampers.getText().toString());
-                float V =  Float.parseFloat(Volts.getText().toString());
-                float summosnost = A * V;
-                Mosnost.setText(Float.toString(summosnost));
+                    //общая мощность
+                    float A = Float.parseFloat(Ampers.getText().toString());
+                    float V = Float.parseFloat(Volts.getText().toString());
+                    float summosnost = A * V;
+                    Mosnost.setText(Float.toString(summosnost));
 
-                //погрешность
-                float pog = (pogresn-summosnost)/summosnost*100;
-                String formatdobl = String.format("%.2f",pog);
-                Itog.setText(formatdobl);
-                break;
+                    //погрешность
+                    float pog = (pogresn - summosnost) / summosnost * 100;
+                    String formatdobl = String.format("%.2f", pog);
+                    Itog.setText(formatdobl);
+                    break;
+
+                } catch (ArithmeticException es) {
+                    Toast.makeText(this, "Ошибка" + es.toString(), Toast.LENGTH_LONG).show();
+                } catch (Exception es) {
+                    Toast.makeText(this, "Проверьте заполненость всех строк", Toast.LENGTH_LONG).show();
+                }
+            case R.id.buttonSumTok:
+                try {
+                    if (et_V1.getText().equals("") || et_V2.getText().equals("") || et_V3.getText().equals("")) {
+                        Toast.makeText(this, "Не должно быть пустого поля", Toast.LENGTH_LONG).show();
+                    }
+                    float a1 = Float.parseFloat(et_V1.getText().toString());
+                    float a2 = Float.parseFloat(et_V2.getText().toString());
+                    float a3 = Float.parseFloat(et_V3.getText().toString());
+                    float SumTok = a1 + a2 + a3;
+
+                    Ampers.setText(Float.toString(SumTok));
+                    break;
+                }catch (Exception e){};
+
+            case R.id.buttonSredVolts:
+                try {
+                    if (et_V1.getText().equals("") || et_V2.getText().equals("") || et_V3.getText().equals("")) {
+                        Toast.makeText(this, "Не должно быть пустого поля", Toast.LENGTH_LONG).show();
+                    }
+                    float v1 = Float.parseFloat(et_V1.getText().toString());
+                    float v2 = Float.parseFloat(et_V2.getText().toString());
+                    float v3 = Float.parseFloat(et_V3.getText().toString());
+                    float sredvolt = (v1 + v2 + v3) / 3;
+                    Volts.setText(Float.toString(sredvolt));
+                    break;
+                }catch (Exception e){};
+            }
         }
     }
 
-    private Runnable updateTimerThread = new Runnable() {
-        public void run() {
-            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
-            updatedTime = timeSwapBuff + timeInMilliseconds;
 
-            int secs = (int) (updatedTime / 1000);
-            int mins = secs / 60;
-            secs = secs % 60;
-            int milliseconds = (int) (updatedTime % 1000);
-            timerValue.setText("" + mins + ":" + String.format("%02d", secs) + ":" + String.format("%03d", milliseconds));
-            customHandler.postDelayed(this, 0);
-        }
-    };
-}
+
+
 
 
 
